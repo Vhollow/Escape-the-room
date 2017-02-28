c0c20b8b4ddcd45ed7563ff1df913ffac49e5b93
Vicente Martínez Franco <vicentemartinezf@gmail.com>
2017-02-28 17:35:38 +0100

    Added Unreal Engine project

---
+++ Escape/Source/Escape/EscapeGameModeBase.cpp
@@ -0,0 +1,8 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Escape.h"
+#include "EscapeGameModeBase.h"
+
+
+
+
---
+++ Escape/Source/Escape/EscapeGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "EscapeGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ESCAPE_API AEscapeGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
